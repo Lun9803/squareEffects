6b1ca1f9c33f14c40b8ad1fe933379231b554dd4
Lun9803 <bolunlun9805@163.com>
2019-05-20 22:02:40 +1000

    added user interface

--- mySketch.js
+++ mySketch.js
@@ -1,18 +1,97 @@
 // Assignment 3 DECO1012
 // FROM Bolun Li   unikey: boli7059
 
-let windowSize = [1440, 960];
+let windowSize = [900, 700];
 let squares = [];
 
+let UIBlock;
+let numSlider;
+let showSliderNum;
+let colorPicker;
+let transparencySlider;
+let sizeSlider;
+
+// setup user interface
+function setupUI(){
+	UIBlock = createDiv();
+	UIBlock.style("display", "block");
+	UIBlock.style("margin", "5px 0");
+	UIBlock.style("height", "50px");
+
+	// numSlider changes the number of rects in a rect group 
+	let numSelection = createDiv();
+	numSelection.parent(UIBlock);
+	numSelection.style("display", "inline-block");
+
+	createDiv("Number of squares").parent(numSelection);
+	numSlider = createSlider(1, 10, 5, 1);
+	numSlider.parent(numSelection);
+	showSliderNum = createSpan(numSlider.value());
+	showSliderNum.parent(numSelection);
+	showSliderNum.style("margin-left", "5px");
+
+	// color picker changes the background color
+	let colorSelection = createDiv();
+	colorSelection.parent(UIBlock);
+	colorSelection.style("display", "inline-block");
+	colorSelection.style("margin-left", "30px");
+
+	createDiv("Background colour").parent(colorSelection);
+	colorPicker = createColorPicker("black");
+	colorPicker.parent(colorSelection);
+	colorPicker.style("display", "block");
+	colorPicker.style("margin", "0 auto");
+
+	// transparency slider changes transparency of the rects
+	let transSelection = createDiv();
+	transSelection.parent(UIBlock);
+	transSelection.style("display", "inline-block");
+	transSelection.style("margin-left", "30px");
+
+	let transDiv = createDiv("Transparency");
+	transDiv.parent(transSelection);
+	transDiv.style("margin-left", "20px");
+
+	transparencySlider = createSlider(0, 1, 0.8, 0.1);
+	transparencySlider.parent(transSelection);
+
+	showTransNum = createSpan(transparencySlider.value());
+	showTransNum.parent(transSelection);
+	showTransNum.style("margin-left", "5px");
+
+	// size slider changes size of the rects
+	let sizeBlock = createDiv();
+	sizeBlock.parent(UIBlock);
+	sizeBlock.style("display", "inline-block");
+	sizeBlock.style("margin-left", "30px");
+
+	let sizeDiv = createDiv("Max rect size");
+	sizeDiv.parent(sizeBlock);
+	sizeDiv.style("margin-left", "20px");
+
+	sizeSlider = createSlider(150, 300, 230, 1);
+	sizeSlider.parent(sizeBlock);
+
+	showSizeNum = createSpan(sizeSlider.value());
+	showSizeNum.parent(sizeBlock);
+	showSizeNum.style("margin-left", "5px");
+}
+
+
 function setup() {
+	setupUI();
 	angleMode(DEGREES);
 	rectMode(CENTER)
 	createCanvas(windowSize[0], windowSize[1]);
 	strokeWeight(0.5);
+
+	// set the onclick function to the canvas so it only works when click inside the panel
+	let canvas = document.getElementsByTagName("canvas")[0];
+	canvas.onclick = function(){createSquares(mouseX, mouseY);}
 }
 
 function draw() {
-	background(255);
+	background(colorPicker.color());
 	let i = 0;
     while(i<squares.length){
 		if(!squares[i].checkExpire()){
@@ -24,6 +103,10 @@ function draw() {
 			squares.splice(i, 1);
 		}
 	}
+
+	showSliderNum.html(numSlider.value());
+	showTransNum.html(transparencySlider.value());
+	showSizeNum.html(sizeSlider.value());
 }
 
 // square class
@@ -33,8 +116,8 @@ function squareObj(x, y, rotation, fillColor, storkeColor){
 	// dimensions & positions of the rectangle
 	this.x = x;
 	this.y = y;
-	this.w = random(130, 200);
-	this.h = random(60, 130)
+	this.w = random(sizeSlider.value()-90, sizeSlider.value());
+	this.h = random(sizeSlider.value()-140, sizeSlider.value()-90);
 
 	// wether the rect rotate clockwise when created
 	this.clockWise = random([true, false]);
@@ -73,9 +156,9 @@ function squareObj(x, y, rotation, fillColor, storkeColor){
 		// do a leave animate, leave animate lasts for 0.5 seconds
 		if(existTime>3000){
 			if(this.clockWise){
-				tempRotation = this.rotation + 90*(1-(existTime-3000)/500)
+				tempRotation = this.rotation + 90*(existTime-3000)/500
 			}else{
-				tempRotation = this.rotation - 90*(1-(existTime-3000)/500)
+				tempRotation = this.rotation - 90*(existTime-3000)/500
 			}
 			tempW = this.w * (1-(existTime-3000)/500);
 			tempH = this.h * (1-(existTime-3000)/500);
@@ -101,18 +184,13 @@ function createSquares(x, y){
 	let mainColor = parseInt(random(360));
 	let rotation = random(90);
 	let strokeColor = color(`hsb(${mainColor}, 50%, 100%)`);
-	for(let i=0; i<4; i++){
+	for(let i=0; i<numSlider.value(); i++){
 		// create square group
-		let fillColor = color(`hsba(${mainColor}, 100%, ${100-i*5}%, 0.2)`);
+		let fillColor = color(`hsba(${mainColor}, 100%, ${100-i*5}%, ${1-transparencySlider.value()})`);
 		// rects does not have exact same center, add randomness
 		let randomX = x+random(-30, 30);
 		let randomY = y+(random(-30, 30));
 		squares.push(new squareObj(randomX, randomY, rotation, fillColor, strokeColor));
 	}
 }
-
-function mouseClicked(){
-	createSquares(mouseX, mouseY);
-}
-
 
