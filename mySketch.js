709f18d260e8a793e2f195f349a8908dc3c434ee
Lun9803 <bolunlun9805@163.com>
2019-05-23 00:29:25 +1000

    added random shapes

--- mySketch.js
+++ mySketch.js
@@ -2,7 +2,7 @@
 // FROM Bolun Li   unikey: boli7059
 
 let windowSize = [900, 700];
-let squares = [];
+let shapes = [];
 
 let UIBlock;
 let numSlider;
@@ -15,7 +15,7 @@ let sizeSlider;
 function setupUI(){
 	UIBlock = createDiv();
 	UIBlock.style("display", "block");
-	UIBlock.style("margin", "5px 0");
+	UIBlock.style("margin", "15px 0 20px");
 	UIBlock.style("height", "50px");
 
 	// numSlider changes the number of rects in a rect group 
@@ -23,7 +23,7 @@ function setupUI(){
 	numSelection.parent(UIBlock);
 	numSelection.style("display", "inline-block");
 
-	createDiv("Number of squares").parent(numSelection);
+	createDiv("Number of particles").parent(numSelection);
 	numSlider = createSlider(1, 10, 5, 1);
 	numSlider.parent(numSelection);
 	showSliderNum = createSpan(numSlider.value());
@@ -65,11 +65,11 @@ function setupUI(){
 	sizeBlock.style("display", "inline-block");
 	sizeBlock.style("margin-left", "30px");
 
-	let sizeDiv = createDiv("Max rect size");
+	let sizeDiv = createDiv("Max shape size");
 	sizeDiv.parent(sizeBlock);
 	sizeDiv.style("margin-left", "20px");
 
-	sizeSlider = createSlider(150, 300, 230, 1);
+	sizeSlider = createSlider(100, 200, 120, 1);
 	sizeSlider.parent(sizeBlock);
 
 	showSizeNum = createSpan(sizeSlider.value());
@@ -87,20 +87,20 @@ function setup() {
 
 	// set the onclick function to the canvas so it only works when click inside the panel
 	let canvas = document.getElementsByTagName("canvas")[0];
-	canvas.onclick = function(){createSquares(mouseX, mouseY);}
+	canvas.onclick = function(){createShapes(mouseX, mouseY);}
 }
 
 function draw() {
 	background(colorPicker.color());
 	let i = 0;
-    while(i<squares.length){
-		if(!squares[i].checkExpire()){
-			squares[i].render();
+    while(i<shapes.length){
+		if(!shapes[i].checkExpire()){
+			shapes[i].render();
 			i++;
 		}
 		// if a square expires, delete it from array
 		else{
-			squares.splice(i, 1);
+			shapes.splice(i, 1);
 		}
 	}
 
@@ -109,41 +109,65 @@ function draw() {
 	showSizeNum.html(sizeSlider.value());
 }
 
-// square class
 
-function squareObj(x, y, rotation, fillColor, storkeColor){
+// draw triangles 
+
+function drawTriangle(w){
+	triangle(0, 0, -w/2, w, w/2, w)
+}
+
+
+// shape class
+
+function particle(x, y, rotation, fillColor, storkeColor, shape){
 	
-	// dimensions & positions of the rectangle
+	// positions of the shape
 	this.x = x;
 	this.y = y;
-	this.w = random(sizeSlider.value()-90, sizeSlider.value());
-	this.h = random(sizeSlider.value()-140, sizeSlider.value()-90);
+
+	// type of the shape & dimension of the shape
+	this.shape = shape;
+	switch(shape){
+		case "rectangle":
+			this.w = random(sizeSlider.value()-90, sizeSlider.value());
+			this.hRatio = random(1, 4); 
+			break;
+		case "triangle":
+			this.w = random(sizeSlider.value()-90, sizeSlider.value());
+			break;
+		case "circle":
+			this.w = random(sizeSlider.value()-90, sizeSlider.value());
+			break;
+		default:
+			break;
+	}
 
 	// wether the rect rotate clockwise when created
 	this.clockWise = random([true, false]);
 
-	// rotation of the rectangle
+	// rotation of the shape
 	this.rotation = rotation+90*parseInt(random(4));
 
-	// color of the rectangle
+	// color of the shape
 	this.fillColor = fillColor;
 	this.strokeColor = storkeColor;
 
-	// the time that the rect is created
+	// the time that the shape is created
 	this.startTime = new Date();
 
-	// checks if the square expires and needs to be removed
+	// checks if the shape expires and needs to be removed
 	this.checkExpire = function(){
 		// expires after 4 seconds
 		return new Date() - this.startTime>3500;
 	}
 
 	this.render = function(){
-		// the time count after the rect is created
+
+		// the time count after the shape is created
 		let existTime = new Date() - this.startTime;
-		let tempRotation = this.rotation;
+
 		let tempW = this.w;
-		let tempH = this.h;
+		let tempRotation = this.rotation;
 		// do a enter animate
 		if(existTime<300){
 			if(this.clockWise){
@@ -151,46 +175,60 @@ function squareObj(x, y, rotation, fillColor, storkeColor){
 			}else{
 				tempRotation = this.rotation + 90*(1-existTime/300)
 			}
+			tempW = this.w * (1-(1-existTime/300));
 		}
 
 		// do a leave animate, leave animate lasts for 0.5 seconds
 		if(existTime>3000){
 			if(this.clockWise){
-				tempRotation = this.rotation + 90*(existTime-3000)/500
-			}else{
 				tempRotation = this.rotation - 90*(existTime-3000)/500
+			}else{
+				tempRotation = this.rotation + 90*(existTime-3000)/500
 			}
 			tempW = this.w * (1-(existTime-3000)/500);
-			tempH = this.h * (1-(existTime-3000)/500);
 		}
 
 		push();
-		
 		fill(this.fillColor);
 		stroke(this.strokeColor);
 		translate(this.x, this.y);
 		rotate(tempRotation);
-		rect(0, 0, tempW, tempH);
+
+		switch(this.shape){
+			case "rectangle":
+				let tempH = this.w/this.hRatio;
+				rect(0, 0, tempW, tempH);
+				break;
+			case "triangle":
+				drawTriangle(tempW/1.5);
+				break;
+			case "circle":
+				circle(0, 0, tempW/2.5);
+				break;
+			default:
+				break;
+		}
 		
 		pop();
 	}
 }
 
 
-// given a coordinator, randomly draw the squares
+// given a coordinator, randomly draw the shapes
 
-function createSquares(x, y){
+function createShapes(x, y){
 	// get a random color & rotation for the squre group 
 	let mainColor = parseInt(random(360));
 	let rotation = random(90);
 	let strokeColor = color(`hsb(${mainColor}, 50%, 100%)`);
+	let shape = random(["rectangle", "triangle", "circle"]);
 	for(let i=0; i<numSlider.value(); i++){
 		// create square group
 		let fillColor = color(`hsba(${mainColor}, 100%, ${100-i*5}%, ${1-transparencySlider.value()})`);
 		// rects does not have exact same center, add randomness
 		let randomX = x+random(-30, 30);
 		let randomY = y+(random(-30, 30));
-		squares.push(new squareObj(randomX, randomY, rotation, fillColor, strokeColor));
+		shapes.push(new particle(randomX, randomY, rotation, fillColor, strokeColor, shape));
 	}
 }
 
